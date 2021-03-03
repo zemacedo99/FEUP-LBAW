683d9ce165ecfedb2e296bb4c36abd7184d52ba1
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-03-03 21:04:54 +0000

    minor changes

--- html/index.php
+++ html/index.php
@@ -27,10 +27,12 @@
       
   <header>
 
-    <nav class="navbar navbar-expand-sm navbar-light">
+    <nav class="navbar navbar-expand-md navbar-light">
       <div class="container-fluid">
       <a class = "navbar-brand" href="index.php"> <img alt="Logo" src="logo.png" width="100" height="25"> </a>
-    
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <!-- <li class="nav-item">
