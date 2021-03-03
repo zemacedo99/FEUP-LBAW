07a02aece40a53ef59d04d9e6610146bb1870819
Ca-moes <andre3000pt@gmail.com>
2021-03-03 11:27:34 +0000

    register page with includes

--- html/index.php
+++ html/index.php
@@ -1,41 +1,61 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+include './common/head.php'
+?>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-7 col-lg-5">
+      <figure class="figure d-flex justify-content-center mt-3 mb-5">
+        <img class="figure-img img-fluid rounded" src="https://via.placeholder.com/300x200" alt="">
+      </figure>
 
-
-    <title>Title of the document</title>
-  </head>
-  <body>
-    <!-- Optional JavaScript; choose one of the two! -->
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
-    <p>Content from HTML.</p>
-    <?php
-      echo '<p>content from PHP.</p>';
-    ?>
-    <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="floatingName" placeholder="John Doe">
+        <label for="floatingName">Name</label>
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingEmail" placeholder="name@example.com">
+        <label for="floatingEmail">Email address</label>
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-  
-  </body>
-</html>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingConfirmPassword" placeholder="Confirm Password">
+        <label for="floatingConfirmPassword">Confirm Password</label>
+      </div>
+
+      <div class="row mt-3 mb-3">
+        <div class="col d-flex justify-content-center">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+              I'm a Customer
+            </label>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-center">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+              I'm a Supplier
+            </label>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="d-grid gap-2 col-6 col-sm-5 col-md-7 mx-auto">
+        <button type="submit" class="btn btn-primary">Sign up</button>
+        <p class="text-muted mb-0 d-flex justify-content-center"> Already have an account? </p>
+        <a href="#" class="link-secondary d-flex justify-content-center">Sign in</a>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<?php
+include './common/end.php'
+?>
