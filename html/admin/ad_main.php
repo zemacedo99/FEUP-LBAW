ca379af31b25c54747067a0cdbf67cee367c6e95
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2021-03-04 14:18:22 -0100

    Developing

--- html/admin/ad_main.php
+++ html/admin/ad_main.php
@@ -8,26 +8,20 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/CSS" href="/admin/admin.css" >
+    <link rel="stylesheet" type="text/CSS" href="admin.css">
 
     <title>Document</title>
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
-    <ul class="nav flex-column justify-content-center" id="naveg">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        
-    </ul>
+    <div class="container" id="vaitefoder">
+        <div class="row py-5 px-1">View suppliers request</div>
+        <div class="row py-5">View products</div>
+        <div class="row py-5">View users </div>
+    </div>
+  
+
 </body>
 
 </html>
