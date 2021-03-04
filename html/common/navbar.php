ce53dab2c2fafa80e6ce1e58909129d7751576a3
Ca-moes <andre3000pt@gmail.com>
2021-03-04 17:53:51 +0000

    all register done

--- html/common/navbar.php
+++ html/common/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="./images/favicon.ico" alt="" width="30" height="24" class="d-inline-block align-top">
+      <img src="../images/favicon.ico" alt="" width="30" height="24" class="d-inline-block align-top">
       MyGarden
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
