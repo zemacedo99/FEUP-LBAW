6c0f1cce77131d1a008d604c70bebfb065615315
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2021-03-05 15:47:51 -0100

    About Us page done

--- html/admin/ad_main.php
+++ html/admin/ad_main.php
@@ -1,137 +1,94 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include '../common/head.php';
 
-<style>
+?>
+<div class="container-fluid mt-5">
+    <div class="row ">
+        <div class="col-3 mx-3 py-5 align-self-center" style="border-right: 1px solid gray; height: 700px;">
 
-</style>
+            <div class="row my-5">View suppliers request</div>
+            <div class="row my-5">View products</div>
+            <div class="row my-5">View users </div>
+        </div>
+        <div class="col-7 ">
+            <h2 class="my-5 text-center">Welcome, admin!</h2>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <form action="">
+                <div class="col-4">
+                    <input class="form-control" placeholder="Search">
+                </div>
+            </form>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link href="../admin.css" rel="stylesheet">
+            <table class="table mt-3">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
 
-    <title>MyGarden</title>
-</head>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">#1</th>
+                        <td>
+                            <div class="row">
+                                <div class="col-7">Zé das bananas</div>
+                                <div class="col-5">
+                                    <button><i class="bi bi-check"></i></button>
+                                    <button><i class="bi bi-x"></i></button>
+                                    <button><i class="bi bi-info-circle"></i></button>
+                                </div>
+                            </div>
+                        </td>
 
+                    </tr>
 
+                    <tr>
+                        <th scope="row">#2</th>
+                        <td>
+                            <div class="row">
+                                <div class="col-7">Luís das beterrabas</div>
+                                <div class="col-5">
+                                    <button><i class="bi bi-check"></i></button>
+                                    <button><i class="bi bi-x"></i></button>
+                                    <button><i class="bi bi-info-circle"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
 
+                    <tr>
+                        <th scope="row">#3</th>
+                        <td>
+                            <div class="row">
+                                <div class="col-7">André dos pêssegos</div>
+                                <div class="col-5">
+                                    <button><i class="bi bi-check"></i></button>
+                                    <button><i class="bi bi-x"></i></button>
+                                    <button><i class="bi bi-info-circle"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
 
-<body>
-
-
-    <div class="container-fluid mt-5">
-        <div class="row mx-auto">
-            <div class="col-3 mx-3 ">
-
-                <div class="row my-5">View suppliers request</div>
-                <div class="row my-5">View products</div>
-                <div class="row my-5">View users </div>
-            </div>
-            <div class="col-7 ">
-                <h2 class="my-5 text-center">Welcome, admin!</h2>
-
-                <form action="">
-                    <div class="col-4">
-                        <input class="form-control" placeholder="Search">
-                    </div>
-                </form>
-
-                <table class="table mt-3">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Name</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">#1</th>
-                            <td>
-                                <div class="row">
-                                    <div class="col-7">Zé das bananas</div>
-                                    <div class="col-5"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                    </svg></button></div>
-                                </div>  
-                            </td>
-
-                        </tr>
-
-                        <tr>
-                            <th scope="row">#2</th>
-                            <td>
-                                <div class="row">
-                                    <div class="col-7">Luís das beterrabas</div>
-                                    <div class="col-5"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                    </svg></button></div>
-                                </div>  
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <th scope="row">#3</th>
-                            <td>
-                                <div class="row">
-                                    <div class="col-7">André dos pêssegos</div>
-                                    <div class="col-5"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                    </svg></button></div>
-                                </div>  
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <th scope="row">#4</th>
-                            <td>
-                                <div class="row">
-                                    <div class="col-7">Ricardo das ananonas</div>
-                                    <div class="col-5"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                                    </svg></button>
-                                <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                    </svg></button></div>
-                                </div>  
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                    <tr>
+                        <th scope="row">#4</th>
+                        <td>
+                            <div class="row">
+                                <div class="col-7">Ricardo das ananonas</div>
+                                <div class="col-5">
+                                    <button><i class="bi bi-check"></i></button>
+                                    <button><i class="bi bi-x"></i></button>
+                                    <button><i class="bi bi-info-circle"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 
 
-</body>
-
-</html>
+<?php include '../common/end.php' ?>
---
+++ html/misc/about_us.php
@@ -0,0 +1,81 @@
+<?php
+include '../common/head.php';
+include '../common/navbar.php';
+?>
+
+
+
+<div class="container">
+    <div class="row mt-3 justify-content-center">
+        <div class="col-2 mx-1"><a class="btn btn-primary" href="#FAQ" role="button">FAQ</a></div>
+        <div class="col-2 ms-1 me-4"><a class="btn btn-primary" href="#contactUs" role="button">Contact Us</a></div>
+        <div class="col-2 mx-1 mb-4"><a class="btn btn-primary" href="#findUs" role="button">Find Us</a></div>
+    </div>
+    <div class="row mt-5 justify-content-center">
+        <div class="col-2 justify-content-center"><img width="200" height="200" src="../images/icon.png" class="img-fluid" alt="Company Logotype"></div>
+        <div class="col-5">
+            <h2>MyGarden</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod teLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                Lorem ipsum dolor sit.Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+        </div>
+    </div>
+    <div class="row justify-content-center" id="FAQ">
+        <div class="col-7 mt-5" style="border-bottom: 1px solid black">
+            <h1>FAQ</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-7 mt-3">
+            <h4>Pergunta muito fixe</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .
+                Lorem ipsum dolor sit amet, consectetur</p>
+        </div>
+        
+        <div class="col-7 mt-3">
+            <h4>Pergunta muito fixe</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .
+                Lorem ipsum dolor sit amet, consectetur</p>
+        </div>
+        <div class="col-7 mt-3">
+            <h4>Pergunta muito fixe</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .
+                Lorem ipsum dolor sit amet, consectetur</p>
+        </div>
+        
+    </div>
+
+    <div class="row justify-content-center" id="contactUs">
+        <div class="col-7 mt-5" style="border-bottom: 1px solid black">
+            <h1>Contact Us</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-7 mt-3">
+            <p class="text-center">mygardensupport@mygarden.pt</p>
+            <p class="text-center">+351 255 420 690</p>
+        </div>
+        
+    </div>
+
+    <div class="row justify-content-center" id="findUs">
+        <div class="col-7 mt-5" style="border-bottom: 1px solid black">
+            <h1>Find Us</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-7 mt-3">
+            <p class="text-center">Rua Dr. Roberto Frias, 4200-465 Porto</p>
+
+        </div>
+        
+    </div>
+
+
+</div>
+
+
+<?php include '../common/end.php' ?>
