f29066e5c951451d18121e59c438ba06f8fc9d9c
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-03-05 18:51:33 +0000

    list all products responsive

--- html/index.php
+++ html/index.php
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
   <!-- material icons-->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -23,49 +22,47 @@
 <body>
   <!-- Optional JavaScript; choose one of the two! -->
   <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
-    crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
   </script>
-    
+
   <header>
 
     <!-- navbar global -->
     <nav class="navbar navbar-expand-md navbar-light">
       <div class="container-fluid">
-        <a class = "navbar-brand" href="index.php"> <img alt="Logo" src="logo.png" width="100" height="25"> </a>
+        <a class="navbar-brand" href="index.php"> <img alt="Logo" src="logo.png" width="100" height="25"> </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <!-- <li class="nav-item">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!-- <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li> -->
-          <li class="nav-item">
-            <a class="nav-link" id = "navLinks" href="supplier_all_prod.php">Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" id  = "navLinks" href="#">Stores</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" id = "navLinks" href="#">About Us</a>
-          </li>
-        </ul>
+            <li class="nav-item">
+              <a class="nav-link" id="navLinks" href="supplier_all_prod.php">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="navLinks" href="#">Stores</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="navLinks" href="#">About Us</a>
+            </li>
+          </ul>
 
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button type="button" id = "headericon">search</button>
-        </form>
-      </div>
-
-      <!-- navbar profile and cart buttons -->
-      <div class="navbar-nav ms-auto">
-        <div class="col align-items-end">
-          <button type="button" id = "headericon">account_circle</button>
-          <button type="button" id = "headericon">shopping_cart</button>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button type="button" id="headericon">search</button>
+          </form>
         </div>
-      </div>
+
+        <!-- navbar profile and cart buttons -->
+        <div class="navbar-nav ms-auto">
+          <div class="col align-items-end">
+            <button type="button" id="headericon">account_circle</button>
+            <button type="button" id="headericon">shopping_cart</button>
+          </div>
+        </div>
     </nav>
 
   </header>
@@ -74,34 +71,34 @@
 
   <!-- Bundle and Cupon buttons -->
   <div class="container">
-    
+
     <!-- vertical line -->
     <div class="position-absolute top-50 start-50 translate-middle">
-      <div id = "vertical_line"></div>
+      <div id="vertical_line"></div>
     </div>
 
     <div class="row mt-3"></div>
     <div class="row mt-3"></div>
     <div class="row mt-3"></div>
 
-    <div class = "row mb-3">
+    <div class="row mb-3">
       <!-- ****************** Left Side ****************** -->
       <div class="col-6">
         <div class="d-flex justify-content-center">
-          <button type="button"  class = "mainbtt"> Bundles</button>
-          <button type="button"  class = "mainbtt" id = "mainbtticon">add</button>
+          <button type="button" class="mainbtt"> Bundles</button>
+          <button type="button" class="mainbtt" id="mainbtticon">add</button>
         </div>
 
         <div class="row mt-3"></div>
-        <div class="row mt-3"></div>    
+        <div class="row mt-3"></div>
         <div class="row mt-3"></div>
 
         <div class="container overflow-hidden">
           <div class="row row-cols-1 row-cols-md-1 g-4">
             <div class="col-6">
-             <div class="p-3">
-              <div class="card mb-3" style=" max-height: 200px; ">
-                  <img src="bananas.jpg" class="card-img-top" alt="bananas" >
+              <div class="p-3">
+                <div class="card mb-3" style=" max-height: 200px; ">
+                  <img src="bananas.jpg" class="card-img-top" alt="bananas">
                   <div class="text-center">
                     <div class="card-img-overlay">
                       <div class="row mt-3"></div>
@@ -111,13 +108,13 @@
                     </div>
                   </div>
                 </div>
-              </div>          
+              </div>
             </div>
 
             <div class="col-6">
               <div class="p-3">
                 <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon" >
+                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
                   <div class="text-center">
                     <div class="card-img-overlay">
                       <div class="row mt-3"></div>
@@ -136,61 +133,46 @@
 
 
       </div>
-      
+
       <!-- ****************** Right Side ****************** -->
       <div class="col-6">
         <div class="d-flex justify-content-center">
-          <button type="button" class = "mainbtt">Cupons</button>
-          <button type="button" class = "mainbtt" id = "mainbtticon">add</button>
+          <button type="button" class="mainbtt">Cupons</button>
+          <button type="button" class="mainbtt" id="mainbtticon">add</button>
         </div>
         <div class="row mt-3"></div>
-        <div class="row mt-3"></div>    
+        <div class="row mt-3"></div>
         <div class="row mt-3"></div>
 
         <div class="container overflow-hidden">
           <div class="row row-cols-1 row-cols-md-2 g-4">
-            
+
             <div class="col-6">
               <div class="p-3">
                 <div class="card ">
-             
-                <img src="cupon_done.jpg" class="card-img-top" alt="cupon" >
-                <div class="text-center">
-                  <div class="card-img-overlay">
-                    <div class="row mt-3"></div>
-                    <div class="row mt-3"></div>
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">10%</p>
-                  </div>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">Valid until 2/5/2020</small>
-                </div>
 
-
-                </div>
-              </div>
-            </div>
-
-            <div class="col-6">    
-              <div class="p-3">
-                  <div class="card ">
-                    <img src="cupon_done.jpg" class="card-img-top" alt="cupon" >
-                    <div class="text-center">
-                      <div class="card-img-overlay">
-                        <div class="row mt-3"></div>
-                        <div class="row mt-3"></div>
-                        <h6 class="card-title">SUMMER 2020</h6>
-                        <p class="card-text">10%</p>
-                      </div>
+                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
+                  <div class="text-center">
+                    <div class="card-img-overlay">
+                      <div class="row mt-3"></div>
+                      <div class="row mt-3"></div>
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">10%</p>
                     </div>
                   </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Valid until 2/5/2020</small>
+                  </div>
+
+
+                </div>
               </div>
             </div>
+
             <div class="col-6">
-             <div class="p-3">
+              <div class="p-3">
                 <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon" >
+                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
                   <div class="text-center">
                     <div class="card-img-overlay">
                       <div class="row mt-3"></div>
@@ -200,12 +182,27 @@
                     </div>
                   </div>
                 </div>
-              </div>          
+              </div>
             </div>
             <div class="col-6">
-             <div class="p-3">
+              <div class="p-3">
                 <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon" >
+                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
+                  <div class="text-center">
+                    <div class="card-img-overlay">
+                      <div class="row mt-3"></div>
+                      <div class="row mt-3"></div>
+                      <h6 class="card-title">SUMMER 2020</h6>
+                      <p class="card-text">10%</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="p-3">
+                <div class="card ">
+                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
                   <div class="text-center">
                     <div class="card-img-overlay">
                       <div class="row mt-3"></div>
--- html/supplier_all_prod.php
+++ html/supplier_all_prod.php
@@ -139,76 +139,77 @@
 
       <!-- List of all products that a store have -->
       <div class="row row-cols-1 row-cols-sm-2 g-4">
-        <div class="col-sm">
-          
-        <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-            <img src="bananas.jpg" class="card-img-top" alt="bananas" >
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Bananas</h5>
-                <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+        
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                  
+          <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+              <img src="bananas.jpg" class="card-img-top" alt="bananas" >
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Bananas</h5>
+                  <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
+                  <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
         </div>
-        <div class="col-sm">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
-        <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-            <img src="batata_vermelha.jpg" class="card-img-top" alt="batatas vermelhas" >
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Batatas vermelhas</h5>
-                <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+          <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+              <img src="batata_vermelha.jpg" class="card-img-top" alt="batatas vermelhas" >
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Batatas vermelhas</h5>
+                  <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
+                  <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
         </div>
-        <div class="col-sm">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
-        <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-            <img src="batata-amarela.jpg" class="card-img-top" alt="batatas amarelas" >
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Batatas amarela</h5>
-                <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+          <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+              <img src="batata-amarela.jpg" class="card-img-top" alt="batatas amarelas" >
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Batatas amarela</h5>
+                  <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
+                  <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
         </div>
-        <div class="col-sm">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
-        <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-            <img src="tomate.jpg" class="card-img-top" alt="Tomate" >
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Tomate</h5>
-                <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+          <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+              <img src="tomate.jpg" class="card-img-top" alt="Tomate" >
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Tomate</h5>
+                  <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
+                  <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
         </div>
       </div>
@@ -217,25 +218,6 @@
 
 
   </header>
-
-  <!-- <footer>
-
-    <div id="navfooter">
-      <img src="logo.png" class="img-fluid" alt="Logo">
-      <div class="d-flex flex-row-reverse">
-        <div class="row">
-          <button type="button" id = "footericon">thumb_up</button>
-          <button type="button" id = "footericon">check_circle_outline</button>
-          <button type="button"  id = "footericon">star_rate</button>
-          <button type="button"  id = "footericon">mdi-bell</button>
-          <button type="button"  id = "footericon">favorite</button>
-        </div>
-    </div>
-    </div>
-    
-  </footer>
- -->
-
 
 
 
