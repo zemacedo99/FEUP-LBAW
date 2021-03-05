52ab8727390d21d666af10ff884d8ec754971a8d
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-03-05 19:54:32 +0000

    bundle and cupons

--- html/index.php
+++ html/index.php
@@ -72,10 +72,11 @@
   <!-- Bundle and Cupon buttons -->
   <div class="container">
 
+
     <!-- vertical line -->
-    <div class="position-absolute top-50 start-50 translate-middle">
+    <!-- <div class="position-absolute top-50 start-50 translate-middle">
       <div id="vertical_line"></div>
-    </div>
+    </div> -->
 
     <div class="row mt-3"></div>
     <div class="row mt-3"></div>
@@ -83,7 +84,8 @@
 
     <div class="row mb-3">
       <!-- ****************** Left Side ****************** -->
-      <div class="col-6">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="border-right:2px solid green;">
+
         <div class="d-flex justify-content-center">
           <button type="button" class="mainbtt"> Bundles</button>
           <button type="button" class="mainbtt" id="mainbtticon">add</button>
@@ -93,49 +95,93 @@
         <div class="row mt-3"></div>
         <div class="row mt-3"></div>
 
-        <div class="container overflow-hidden">
-          <div class="row row-cols-1 row-cols-md-1 g-4">
-            <div class="col-6">
-              <div class="p-3">
-                <div class="card mb-3" style=" max-height: 200px; ">
-                  <img src="bananas.jpg" class="card-img-top" alt="bananas">
-                  <div class="text-center">
-                    <div class="card-img-overlay">
-                      <div class="row mt-3"></div>
-                      <div class="row mt-3"></div>
-                      <h6 class="card-title">SUMMER 2020</h6>
-                      <p class="card-text">10%</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style='border:1px solid silver;'>
+          <div class="row">
+
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="bananas.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
             </div>
 
-            <div class="col-6">
-              <div class="p-3">
-                <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
-                  <div class="text-center">
-                    <div class="card-img-overlay">
-                      <div class="row mt-3"></div>
-                      <div class="row mt-3"></div>
-                      <h6 class="card-title">SUMMER 2020</h6>
-                      <p class="card-text">10%</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="batata_vermelha.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="tomate.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="batata_vermelha.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="bananas.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+          </div>
+
+        </div>
+        <div class="row">
+
+          <div class="col-6">
+            <div class="d-flex justify-content-start">Winter Bundle</div>
+          </div>
+
+          <div class="col-6">
+            <div class="d-flex justify-content-end">30.50€</div>
+          </div>
+
+        </div>
+        <br><br>
+
+
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style='border:1px solid silver;'>
+          <div class="row">
+
+          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="bananas.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="batata_vermelha.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="tomate.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="batata_vermelha.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
+              <img src="bananas.jpg" alt="bananas" style=" margin-left:auto; margin-right:auto;width:104px;height:142px;">
             </div>
 
 
           </div>
         </div>
+        <div class="row">
+
+          <div class="col-6">
+            <div class="d-flex justify-content-start">Winter Bundle</div>
+          </div>
+
+          <div class="col-6">
+            <div class="d-flex justify-content-end">30.50€</div>
+          </div>
+
+        </div>
+        <br><br>
 
 
       </div>
 
       <!-- ****************** Right Side ****************** -->
-      <div class="col-6">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <div class="d-flex justify-content-center">
           <button type="button" class="mainbtt">Cupons</button>
           <button type="button" class="mainbtt" id="mainbtticon">add</button>
@@ -144,83 +190,86 @@
         <div class="row mt-3"></div>
         <div class="row mt-3"></div>
 
-        <div class="container overflow-hidden">
-          <div class="row row-cols-1 row-cols-md-2 g-4">
 
-            <div class="col-6">
-              <div class="p-3">
-                <div class="card ">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
 
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="p-3">
+              <div class="card">
+                <img src="cupon.jpg" class="card-img" alt="cupon">
+                <div class="card-img-overlay">
                   <div class="text-center">
-                    <div class="card-img-overlay">
-                      <div class="row mt-3"></div>
-                      <div class="row mt-3"></div>
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">10%</p>
-                    </div>
-                  </div>
-                  <div class="card-footer">
-                    <small class="text-muted">Valid until 2/5/2020</small>
-                  </div>
-
-
-                </div>
-              </div>
-            </div>
-
-            <div class="col-6">
-              <div class="p-3">
-                <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
-                  <div class="text-center">
-                    <div class="card-img-overlay">
-                      <div class="row mt-3"></div>
-                      <div class="row mt-3"></div>
-                      <h6 class="card-title">SUMMER 2020</h6>
-                      <p class="card-text">10%</p>
-                    </div>
+                    <br><br>
+                    <h5 class="card-title">SUMMER 2021</h5>
+                    <p class="card-text">10%</p>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="p-3">
-                <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
-                  <div class="text-center">
-                    <div class="card-img-overlay">
-                      <div class="row mt-3"></div>
-                      <div class="row mt-3"></div>
-                      <h6 class="card-title">SUMMER 2020</h6>
-                      <p class="card-text">10%</p>
-                    </div>
-                  </div>
+                <div class="card-footer">
+                  <small class="text-muted">Valid until 2/5/2021</small>
                 </div>
               </div>
-            </div>
-            <div class="col-6">
-              <div class="p-3">
-                <div class="card ">
-                  <img src="cupon_done.jpg" class="card-img-top" alt="cupon">
-                  <div class="text-center">
-                    <div class="card-img-overlay">
-                      <div class="row mt-3"></div>
-                      <div class="row mt-3"></div>
-                      <h6 class="card-title">SUMMER 2020</h6>
-                      <p class="card-text">10%</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
             </div>
           </div>
+
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="p-3">
+              <div class="card">
+                <img src="cupon_done.jpg" class="card-img" alt="cupon">
+                <div class="card-img-overlay">
+                  <div class="text-center">
+                    <br><br>
+                    <h5 class="card-title">SUMMER 2020</h5>
+                    <p class="card-text">10%</p>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Valid until 2/5/2020</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="p-3">
+              <div class="card">
+                <img src="cupon_done.jpg" class="card-img" alt="cupon">
+                <div class="card-img-overlay">
+                  <div class="text-center">
+                    <br><br>
+                    <h5 class="card-title">SUMMER 2019</h5>
+                    <p class="card-text">10%</p>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Valid until 2/5/2019</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="p-3">
+              <div class="card">
+                <img src="cupon_done.jpg" class="card-img" alt="cupon">
+                <div class="card-img-overlay">
+                  <div class="text-center">
+                    <br><br>
+                    <h5 class="card-title">SUMMER 2018</h5>
+                    <p class="card-text">10%</p>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Valid until 2/5/2018</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-
-
       </div>
+
+
+
     </div>
+  </div>
   </div>
 
 
