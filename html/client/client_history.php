1fd951baee72dfb82701035b9e2b359bd3b83b7f
Ca-moes <andre3000pt@gmail.com>
2021-03-05 19:37:30 +0000

    Client purchase history

--- html/client/client_history.php
+++ html/client/client_history.php
@@ -1,1 +1,83 @@
-Purchase History Info
+<div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-4 col-md-3 col-lg-2 col-xl-2">
+            <img src="https://via.placeholder.com/150x150" alt="...">
+        </div>
+        <div class="col-8 col-md-9 col-lg-10 col-xl-10">
+            <div class="card-body">
+                <div class="d-flex justify-content-between justify-content-md-start">
+                    <h4 class="card-title">Maças Verdes</h4>
+                    <i class="bi bi-exclamation-triangle ps-md-4"></i>
+                </div>
+                <h6 class="card-subtitle text-muted">4,50€/kg</h6>
+
+                <div class="row row-cols-1 row-cols-md-2">
+                    <p class="card-text text-truncate d-none d-md-block order-md-1 col-md-9 col-lg-9">
+                        Mesmo interessante este texto sobre maças que o fornecedor foi generoso o suficiente de disponiblizar a dar alguma informação sobre o produto
+                    </p>
+                    <h4 class="card-title text-end text-md-start order-md-3">3,33€</h4>
+                    <div class="text-center order-md-2 col-md-3 col-lg-3">
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-x"></i> Cancel Order</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-4 col-md-3 col-lg-2 col-xl-2">
+            <img src="https://via.placeholder.com/150x150" alt="...">
+        </div>
+        <div class="col-8 col-md-9 col-lg-10 col-xl-10">
+            <div class="card-body">
+                <div class="d-flex justify-content-between justify-content-md-start">
+                    <h4 class="card-title">Maças Vermelhas</h4>
+                    <i class="bi bi-cart-plus ps-md-4"></i>
+                </div>
+                <h6 class="card-subtitle text-muted">3,50€/kg</h6>
+
+                <div class="row row-cols-1 row-cols-md-2">
+                    <p class="card-text text-truncate d-none d-md-block order-md-1 col-md-9 col-lg-9">
+                        Este é outro texto mega interessante sobre maças vermelhas, boas para comer cruas ou assadas, possuem tamanhos variados, mas parece que a descrição não cabe aqui então vou terminar.
+                    </p>
+                    <h4 class="card-title text-end text-md-start order-md-3">2,50€</h4>
+                    <div class="text-center order-md-2 col-md-3 col-lg-3">
+                        <button type="button" class="btn btn-primary"><i class="bi bi-list"></i> Edit Review</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-4 col-md-3 col-lg-2 col-xl-2">
+            <img src="https://via.placeholder.com/150x150" alt="...">
+        </div>
+        <div class="col-8 col-md-9 col-lg-10 col-xl-10">
+            <div class="card-body">
+                <div class="d-flex justify-content-between justify-content-md-start">
+                    <h4 class="card-title">Maças Azuis</h4>
+                    <i class="bi bi-cart-plus ps-md-4"></i>
+                </div>
+                <h6 class="card-subtitle text-muted">7,80€/kg</h6>
+
+                <div class="row row-cols-1 row-cols-md-2">
+                    <p class="card-text text-truncate d-none d-md-block order-md-1 col-md-9 col-lg-9">
+                    Maças azuis? Sim caro cliente, maças azuis. Após um tratamento químico altamente tóxico as nossas maças ficam com uma bela cor azul fluorescente.
+                    </p>
+                    <h4 class="card-title text-end text-md-start order-md-3">4,13€</h4>
+                    <div class="text-center order-md-2 col-md-3 col-lg-3">
+                        <button type="button" class="btn btn-success"><i class="bi bi-plus"></i> Leave a Review</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
