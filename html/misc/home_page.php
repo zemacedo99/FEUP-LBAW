4c282ac71a3f841f99d28dd2c0b1ac509d8b3cfc
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2021-03-07 11:17:26 -0100

    Working on home page

---
+++ html/misc/home_page.php
@@ -0,0 +1,18 @@
+<?php
+include '../common/head.php';
+include '../common/navbar.php';
+?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center"> <img src="../images/garden.jpg" alt="Photo of a garden" width="650px" height="400px"></div>
+    </div>
+    <div class="row my-5 justify-content-center">
+        <div class="col-6 text-start">
+            <p><i class="bi bi-clock"></i>   Almost Sold out</p>    
+        </div>
+    </div>
+    
+
+</div>
