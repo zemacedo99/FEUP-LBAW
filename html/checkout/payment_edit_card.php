bec95532fe6a400f0a16064194a1b853af934dd1
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-03-07 19:29:45 +0000

    add and edit a payment card

---
+++ html/checkout/payment_edit_card.php
@@ -0,0 +1,38 @@
+<?php
+include '../common/head.php';
+include '../common/navbar.php';
+include 'payment_info.php';
+?>
+
+<div class="row mt-3"></div>
+<div class="row mt-3"></div>
+<div class="row mt-3"></div>
+
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="row">
+        <div class="d-flex justify-content-center">
+            <button type="button" class="btn btn-secondary btn-sm"><i class="bi bi-trash"></i> Delete Account</button>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-3"></div>
+<div class="row mt-3"></div>
+<div class="row mt-3"></div>
+<div class="row mt-3"></div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="row">
+        <div class="d-flex justify-content-center">
+            <button type="button" class="mainbtt"> <a id="navLinks" href="shipping_payment.php">Continue</a></button>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+include '../common/end.php'
+?>
