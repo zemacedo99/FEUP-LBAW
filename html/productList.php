b26edd6e1462cc6b82e3a2f85c6d145efd45380e
LuisMMMTS <torres.luismiguel@gmail.com>
2021-03-08 00:38:04 +0000

    changes in productList order by menu

--- html/productList.php
+++ html/productList.php
@@ -360,11 +360,13 @@
             </div>
 
             <div class="col-md-1 ms-sm-auto col-lg-2 px-md-1">
-                <select class="form-select" aria-label="Default select example">
+                <select class="form-select" aria-label="Relevance">
                     <option selected>Order by:</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option value="Price Up">Price Up</option>
+                    <option value="Price Down">Price Down</option>
+                    <option value="Review Up">Review Up</option>
+                    <option value="Review Down">Review Down</option>
+                    <option value="Relevance">Relevance</option>
                 </select>
             </div>
         </div>
