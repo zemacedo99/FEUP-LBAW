fc7c2153ea1cdabb1cfa9092873b8e1b38455a46
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-03-08 17:11:17 +0000

    change on all products

--- html/supplier/all_products.php
+++ html/supplier/all_products.php
@@ -175,8 +175,45 @@ include '../common/navbar.php';
             </div>
 
         </div>
-    </div>
 
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+
+            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../images/tomate.jpg" class="card-img-top" alt="Tomate">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Tomate</h5>
+                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
+                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+
+            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../images/tomate.jpg" class="card-img-top" alt="Tomate">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Tomate</h5>
+                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
+                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+<!-- 
     <div class="position-absolute top-100 start-50 translate-middle">
         <nav aria-label="Page navigation example">
             <ul class="pagination">
@@ -195,7 +232,7 @@ include '../common/navbar.php';
                 </li>
             </ul>
         </nav>
-    </div>
+    </div> -->
 </div>
 
 <?php
