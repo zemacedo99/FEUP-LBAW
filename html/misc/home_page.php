5dda9d178d9a0f5b7b0507bd4294f771f9cd7a91
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2021-03-08 16:42:49 -0100

    Fixed homepage

--- html/misc/home_page.php
+++ html/misc/home_page.php
@@ -1,11 +1,43 @@
+<!DOCTYPE html>
+<html class="h-100">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="homepage.css">
+  
+  <!-- FavIcon -->
+  <link rel="shortcut icon" href="../images/favicon.ico" type="image/png">
+  <link rel="icon" href="../images/favicon.ico" type="image/png">
+
+  <link rel="stylesheet" type="text/css" href="../style.css">
+  <script src="../client_profile.js" defer></script>
+
+  <!-- Material icons-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
+  <title>MyGarden</title>
+</head>
+
+<body class="h-100 d-flex flex-column">
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
 
 <?php
-include '../common/head.php';
 include '../common/navbar.php';
 ?>
 
 <div class="container">
-    <div class="row">
+    <div class="row mt-5">
         <div class="col-12 text-center"> <img src="../images/garden.jpg" alt="Photo of a garden" width="650px" height="400px"></div>
     </div>
     <div class="row mt-5">
