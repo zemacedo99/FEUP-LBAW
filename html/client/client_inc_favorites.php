73d339eb72ef3d51f7e5ddab0f5fcb91c6050d2b
Ca-moes <andre3000pt@gmail.com>
2021-03-11 14:51:05 +0000

    animated heart like

--- html/client/client_inc_favorites.php
+++ html/client/client_inc_favorites.php
@@ -1,54 +1,15 @@
+<?php include_once './client_inc_fav_product.php'; ?>
 <div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-4">
-    <div class="col">
-        <div class="card">
-            <img src="https://via.placeholder.com/100x100" class="card-img-top" alt="...">
-            <div class="card-img-overlay d-flex justify-content-end">
-                <i class="bi bi-heart"></i>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Maças verdes</h5>
-                <p class="card-text d-none d-md-block">Aqui fica a descrição deste belo produto, fica um bocado mais curta ou até mesmo cortada</p>
-                <h6 class="card-title text-end text-md-start order-md-3">1,20€/kg</h6>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <img src="https://via.placeholder.com/100x100" class="card-img-top" alt="...">
-            <div class="card-img-overlay d-flex justify-content-end">
-                <i class="bi bi-heart-fill"></i>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Maças verdes</h5>
-                <p class="card-text d-none d-md-block">Aqui fica a descrição deste belo produto, fica um bocado mais curta ou até mesmo cortada</p>
-                <h6 class="card-title text-end text-md-start order-md-3">1,20€/kg</h6>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <img src="https://via.placeholder.com/100x100" class="card-img-top" alt="...">
-            <div class="card-img-overlay d-flex justify-content-end">
-                <i class="bi bi-heart"></i>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Maças verdes</h5>
-                <p class="card-text d-none d-md-block">Aqui fica a descrição deste belo produto, fica um bocado mais curta ou até mesmo cortada</p>
-                <h6 class="card-title text-end text-md-start order-md-3">1,20€/kg</h6>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <img src="https://via.placeholder.com/100x100" class="card-img-top" alt="...">
-            <div class="card-img-overlay d-flex justify-content-end">
-                <i class="bi bi-heart-fill"></i>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Maças verdes</h5>
-                <p class="card-text d-none d-md-block">Aqui fica a descrição deste belo produto, fica um bocado mais curta ou até mesmo cortada</p>
-                <h6 class="card-title text-end text-md-start order-md-3">1,20€/kg</h6>
-            </div>
-        </div>
-    </div>
+    <?php 
+    for ($i=0; $i < 10; $i++) { 
+        fav_card(
+            $i,
+            "Maças verdes",
+            "Aqui fica a descrição deste belo produto, fica um bocado mais curta ou até mesmo cortada",
+            "1,20",
+            "kg"
+        );
+    }
+    ?>
+
 </div>
