0049f8e02020ca805c43e5f4625f670d5eae38ad
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-03-11 19:21:08 +0000

    supplier all pruducts

--- html/supplier/supplier_all_products.php
+++ html/supplier/supplier_all_products.php
@@ -1,33 +1,18 @@
 <?php
 include_once '../common/extras.php';
+include_once 'supplier_inc_all_products.php';
 pageHeader("MyGarden - Products");
 navbar();
 ?>
 
 <div class="container">
 
-    <div class="row mt-3"></div>
-    <div class="row mt-3"></div>
-
-    <div class="row ">
-        <div class="col-12 col-lg-5">
-
-            <div class="row d-flex justify-content-center mb-3">
-                <div class="col-12 col-lg-6" style="width: 15rem;">
-                    <img src="../images/batata-amarela.jpg" class="rounded-circle img-fluid">
-                </div>
-                <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center justify-content-lg-start">
-                    <p class="col-md-auto text-decoration-underline text-center">
-                    <h3>Quinta do Bill </h3>
-                    </p>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="row mt-3"></div>
-    <div class="row mt-3"></div>
+    <?php
+    perfilSupplier(
+        "img_avatar.png",
+        "Quinta do Bill"
+    );
+    ?>
 
     <h3> All Products </h3>
     <div class="row mt-3"></div>
@@ -37,115 +22,25 @@ navbar();
     <!-- List of all products that a store have -->
     <div class="row row-cols-1 row-cols-sm-2 g-4">
 
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <?php
+        for ($i = 0; $i < 5; $i++) {
+            productCard(
+                "bananas",
+                "7,80",
+                "kg",
+                "Na Quinta do Bill tens bananas que te fazem voar, voar, voar."
+            );
+        }
+        for ($i = 0; $i < 5; $i++) {
+            productCard(
+                "batata-amarela",
+                "7,80",
+                "kg",
+                "Na Quinta do Bill tens bananas que te fazem voar, voar, voar."
+            );
+        }
+        ?>
 
-            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="../images/bananas.jpg" class="card-img-top" alt="bananas">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Bananas</h5>
-                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
-            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="../images/batata_vermelha.jpg" class="card-img-top" alt="batatas vermelhas">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Batatas vermelhas</h5>
-                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
-            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="../images/batata-amarela.jpg" class="card-img-top" alt="batatas amarelas">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Batatas amarela</h5>
-                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
-            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="../images/tomate.jpg" class="card-img-top" alt="Tomate">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Tomate</h5>
-                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
-            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="../images/tomate.jpg" class="card-img-top" alt="Tomate">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Tomate</h5>
-                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
-            <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="../images/tomate.jpg" class="card-img-top" alt="Tomate">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Tomate</h5>
-                            <p class="card-text"><small class="text-muted">7.80€/kg</small></p>
-                            <p class="card-text">Na Quinta do Bill tens bananas que te fazem voar, voar, voar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
     </div>
 
 </div>
---
+++ html/supplier/supplier_inc_all_products.php
@@ -0,0 +1,26 @@
+<?php
+function productCard($name, $price, $unit,$description)
+{
+?>
+    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 mx-auto">
+
+        <div class="card h-100" style="width: 540px; height: 200px; max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../images/<?= $name ?>.jpg" class="card-img-top" alt=<?= $name ?> style="margin-left:auto; margin-right:auto;width:8em;height:8em;">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $name ?></h5>
+                        <p class="card-text"><small class="text-muted"><?= $price ?>€/<?= $unit ?></small></p>
+                        <p class="card-text"><?= $description ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<?php
+}
+?>
