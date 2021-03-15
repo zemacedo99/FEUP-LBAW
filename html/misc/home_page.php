1182e22b74ce25788d5d090a60c7a625555bacd4
Ca-moes <andre3000pt@gmail.com>
2021-03-15 12:03:08 +0000

    homepage cleanup

--- html/misc/home_page.php
+++ html/misc/home_page.php
@@ -23,8 +23,6 @@ navbar();
     <?php 
         product_row("New", "newspaper");
     ?>
-
-
     
     <div class="row my-5">
         <div class="col"></div>
@@ -33,25 +31,6 @@ navbar();
         </div>
     </div>
 
-</div>
-
-
-<div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <p class="modal-title"></p>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
 </div>
 
 <?php
