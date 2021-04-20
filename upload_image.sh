d8fc057dd51d5b9592dda5bd511025f5837c427a
LuisMMMTS <torres.luismiguel@gmail.com>
2021-04-20 16:48:23 +0100

    laravel

--- upload_image.sh
+++ upload_image.sh
@@ -5,8 +5,13 @@ read -p "Press [Enter] key to start backup..."
 # Stop execution if a step fails
 set -e
 
-DOCKER_USERNAME=lbaw2131 
+DOCKER_USERNAME=lbaw2131
 IMAGE_NAME=lbaw2131-piu
+
+# Ensure that dependencies are available
+composer install
+php artisan clear-compiled
+php artisan optimize
 
 # necessário dar: `docker login` na máquina
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
