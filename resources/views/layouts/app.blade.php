564336bc3e789d9862d36d735c318ef6a5ffac00
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-04-23 17:31:59 +0100

    changing layout, navbar, about us

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
+
+<head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,26 +13,44 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
+    <!-- Material icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- FavIcon -->
+    <link rel="shortcut icon" href="../images/favicon.ico" type="image/png">
+    <link rel="icon" href="../images/favicon.ico" type="image/png">
+
+    <!-- Self included style and scripts -->
+    <link rel="stylesheet" type="text/css" href="{{asset{'css/style.css'}}}">
+
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
+
     </script>
     <script type="text/javascript" src={{ asset('js/app.js') }} defer>
-</script>
-  </head>
-  <body>
+    </script>
+</head>
+
+<body>
     <main>
-      <header>
-        <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
-        @if (Auth::check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-        @endif
-      </header>
-      <section id="content">
-        @yield('content')
-      </section>
+        <header>
+            <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
+            @if (Auth::check())
+                <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
+            @endif
+        </header>
+        <section id="content">
+            @yield('content')
+        </section>
     </main>
-  </body>
+</body>
+
 </html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-md navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../index.php"> <img alt="Logo" src="../images/logo.png" width="100" height="25"> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" id="navLinks" href="../misc/products_list.php">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="navLinks" href="../misc/products_list.php">Stores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="navLinks" href="../misc/about_us.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="navLinks" href="../misc/site_map.php">SiteMap</a>
+                </li>
+            </ul>
+
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button type="button" id="headericon">search</button>
+            </form>
+        </div>
+
+        <!-- navbar profile and cart buttons -->
+        <div class="navbar-nav ms-auto">
+            <div class="col align-items-end">
+                <button type="button" id="headericon" data-bs-toggle="modal" data-bs-target="#loginModal">account_circle</button>
+                <a href="../checkout/cart_info.php"><button type="button" id="headericon">shopping_cart</button></a>
+            </div>
+        </div>
+</nav>
