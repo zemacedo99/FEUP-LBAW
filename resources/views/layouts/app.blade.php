8a8ce396e223bf5a254b9e3d551245d131e967e6
JosA(c) AntA3nio Dantas Macedo <up201705226@fe.up.pt>
2021-04-23 17:37:58 +0100

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,18 +39,16 @@
     </script>
 </head>
 
-<body>
-    <main>
-        <header>
-            <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
-            @if (Auth::check())
-                <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-            @endif
-        </header>
-        <section id="content">
-            @yield('content')
-        </section>
-    </main>
+<body>  
+  
+  @include('partials.navbar')
+
+  @yield('content')
+  
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
